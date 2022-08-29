1e1003047334f72246a2b7e48d9185d6efdc9872
Ass NIANG <nianga@ept.sn>
2022-08-29 11:18:55 +0000

    adjust textarea input for post creation

--- o2a/src/app/components/create-post/create-post.component.ts
+++ o2a/src/app/components/create-post/create-post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-post',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  onSave(send : NgForm){
+    /**
+     * must save the post content to the db.
+     */
+    alert(`user n-${this.router.url.split("/")[2]} sent ${send.value.content}`); // just for test
+    send.reset();
+  }
 
   ngOnInit(): void {
   }
