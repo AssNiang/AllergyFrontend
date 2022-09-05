843c3c175d83ce9054b8cdc1d59b78e0d81efa5e
Khairy01 <skhairy@ept.sn>
2022-09-05 16:45:11 +0000

    test4_5sept

--- o2a/src/app/app.component.spec.ts
+++ o2a/src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('o2a');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('o2a app is running!');
-  });
 });
